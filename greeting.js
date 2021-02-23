72b278affa29b450a8ffac2fed0fe0d072479fdc
Justin Cepeda <justin.cepeda16@gmail.com>
2021-02-22 21:57:27 -0700

    Changed the vars to functions

--- greeting.js
+++ greeting.js
@@ -1,17 +1,32 @@
 var today = new Date();
 var hourNow = today.getHours();
-var greeting;
-var person = prompt("Please enter your name", "Harry Potter");
 
 
-if (hourNow > 18) {
-    greeting = 'Good evening';
-} else if (hourNow > 12) {
-    greeting = 'Good afternoon';
-} else if (hourNow > 0) {
-    greeting = 'Good morning';
-} else {
-    greeting = 'Welcome';
+ function getName() { 
+     return prompt("Please enter your name", "Ron Weasley");
 }
 
-document.write('<h2>' + greeting + ' ' + person + '!' + '</h2>');
+function getGreeting() {
+if (hourNow > 18) {
+    return 'Good evening';}
+else if (hourNow > 12) {
+    return 'Good afternoon';}
+else if (hourNow > 0) {
+    return 'Good morning';} 
+else {
+    return 'Welcome';}
+}
+
+document.write('<h2>' + getGreeting() + ' ' + getName() + '!' + '</h2>');
+
+// if (hourNow > 18) {
+//     greeting = 'Good evening';
+// } else if (hourNow > 12) {
+//     greeting = 'Good afternoon';
+// } else if (hourNow > 0) {
+//     greeting = 'Good morning';
+// } else {
+//     greeting = 'Welcome';
+// }
+
+// var person = prompt("Please enter your name", "Harry Potter");
--- pressAButton.js
+++ pressAButton.js
@@ -1,1 +1,11 @@
-confirm("Press a button!");
+// confirm("Press a button!");
+
+function pressAButton() {
+    let response = confirm("Press one of the buttons!");
+    if (response === true) {
+        return 'Cool';}
+    else if (response === false) {
+        return 'Not Cool';}
+}
+
+document.write(pressAButton());
